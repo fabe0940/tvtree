793a1c1c5ea022e5d9dfc4ce2454af1fe58e60b6
Mason Fabel <mason.fabel@gmail.com>
2014-04-17 12:07:14 -0700

    Removed tree test code
    Removed extraneous code to make room for futre expansion.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,9 @@
-#include <cstdlib>
-#include <ctime>
 #include <iostream>
+#include <cstdlib>
 #include "BSTree2.h"
 
 using namespace std;
 
 int main(void) {
-	const int MAX = 20;
-	int i;
-	int j;
-	int val;
-	BinarySearchTree* tree;
-
-	srand(time(NULL));
-
-	for(i = 0; i < MAX; i++) {
-		cout << "Test " << (i + 1) << " of " << MAX << endl;
-
-		tree = new BinarySearchTree();
-
-		for(j = 0; j < i; j++) {
-			val = rand() % 100;
-			cout << "[" << (i + 1) << "] Inserting " << val << endl;
-			tree->addNode(val);
-		}
-
-		tree->printBackwardPostOrder();
-
-		delete tree;
-	}
-
 	exit(0);
 }
